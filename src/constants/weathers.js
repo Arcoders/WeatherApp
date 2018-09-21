a961f9c094cbac54fb5007564ebdf24a67b249bd
arcoders <anonismaa@gmail.com>
2018-09-22 00:21:00 +0200

    first commit

---
+++ src/constants/weathers.js
@@ -0,0 +1,19 @@
+const CLOUD = "cloud";
+const CLOUDY = "cloudy";
+const SUN = "sun";
+const RAIN = "rain";
+const SNOW = "snow";
+const WINDY = "windy";
+const THUNDER = "thunder";
+const DRIZZLE = "drizzle"
+
+module.exports = {
+    CLOUD,
+    SUN,
+    CLOUDY,
+    RAIN,
+    SNOW,
+    WINDY,
+    THUNDER,
+    DRIZZLE
+};
